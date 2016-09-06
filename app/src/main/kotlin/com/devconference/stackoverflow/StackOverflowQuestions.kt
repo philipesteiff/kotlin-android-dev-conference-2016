5bc46ee4c9432b82ab95b35333478f0140e75884
Filipe Guedes <fgs.guedes@gmail.com>
2016-09-06 09:40:10 -0300

    Removing commends

--- app/src/main/kotlin/com/devconference/stackoverflow/StackOverflowQuestions.kt
+++ app/src/main/kotlin/com/devconference/stackoverflow/StackOverflowQuestions.kt
@@ -2,17 +2,6 @@ package com.devconference.stackoverflow
 
 import com.google.gson.annotations.SerializedName
 
-/**
- * Compilador já gera:
- * - equals()/hashCode()
- * - toString()
- * - copy()
- *
- * Para garantir consistencias no código gerado:
- * - O construtor primario precisa no mínimo um parametro.
- * - Os parametros precisam ser marcados com `val` ou `var`
- *
- */
 data class StackOverflowQuestions(
     @SerializedName("items") val items: List<StackOverflowQuestion> = emptyList()
 )
